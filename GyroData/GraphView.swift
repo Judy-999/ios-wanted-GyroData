dd307c94cc3274a1a3d8cf7fb9c4defaed67ff7c
Judy <joo00ee@naver.com>
2022-12-28 14:30:32 +0900

    feat: 가속도 측정할 때 GraphView에 그래프 그리는 로직 구현 #6
    - x, y, z를 넘겨 받아 새로운 GraphSegment로 추가

--- GyroData/GraphView.swift
+++ GyroData/GraphView.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class GraphView: UIView {
+    private var segments = [GraphSegment]()
+    private var currentSegment: GraphSegment? {
+        return segments.last
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -29,4 +34,38 @@ class GraphView: UIView {
         
         context.drawGridGraph(in: self.bounds.size)
     }
+    
+    func add(_ motions: [Double]) {
+        segments.forEach {
+            $0.center.x += bounds.size.width / 600.0
+        }
+        
+        addSegment()
+        currentSegment?.add(motions)
+    }
+    
+    // TODO: 모든 세그먼트와 그림 삭제하기
+    func clearSegmanet() {
+        
+    }
+    
+    private func addSegment() {
+        let startPoint: [Double]
+        
+        if let currentSegment = currentSegment {
+            guard let lastPoint = currentSegment.dataPoints.last else { return }
+            startPoint = lastPoint
+        } else {
+            startPoint = [0, 0, 0]
+        }
+        
+        let segmentWidth = CGFloat(32)
+        let newSegment = GraphSegment(startPoint: startPoint, segmentSize: bounds.size.width / 600.0)
+        segments.append(newSegment)
+        
+        newSegment.backgroundColor = .clear
+        newSegment.frame = CGRect(x: -segmentWidth, y: 0, width: segmentWidth, height: bounds.size.height)
+        self.addSubview(newSegment)
+    }
 }
+
